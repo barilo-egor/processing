feb1272951de2294e85ffa8b27a2b5a115ae8391
barilo <barilo.egor@gmail.com>
2026-08-10 13:46:01 +0300

    перенос app класса

--- processing-app/src/main/java/tgb/cryptoexchange/AppApplication.java
+++ processing-app/src/main/java/tgb/cryptoexchange/AppApplication.java
@@ -1,4 +1,4 @@
-package tgb.cryptoexchange.app;
+package tgb.cryptoexchange;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
