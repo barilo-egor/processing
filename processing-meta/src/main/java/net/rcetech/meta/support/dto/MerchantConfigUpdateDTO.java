37a2b4e103ef88cbbda58c25bb5f598e1e9fe00a
barilo <barilo.egor@gmail.com>
2026-09-01 23:00:25 +0300

    мерчант удален из ДТО для обновления

--- processing-meta/src/main/java/net/rcetech/meta/support/dto/MerchantConfigUpdateDTO.java
+++ processing-meta/src/main/java/net/rcetech/meta/support/dto/MerchantConfigUpdateDTO.java
@@ -1,10 +1,7 @@
 package net.rcetech.meta.support.dto;
 
-import tgb.cryptoexchange.commons.enums.Merchant;
-
 public record MerchantConfigUpdateDTO(
         Boolean isOn,
-        Merchant merchant,
         Integer maxAmount,
         Integer minAmount
 ) {}
