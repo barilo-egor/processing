2c3100755b32ad51db591408aea5b0e8a8970e0f
vdovin <V.Vdovin@isida.by>
2026-09-02 12:01:50 +0300

    fix Объединил GrpcConfig

--- processing-api/src/main/java/net/rcetech/api/config/GrpcConfig.java
+++ processing-api/src/main/java/net/rcetech/api/config/GrpcConfig.java
@@ -1,19 +1,38 @@
 package net.rcetech.api.config;
 
 import io.grpc.Channel;
+import net.rcetech.grpc.generated.MerchantConfigServiceGrpc;
 import net.rcetech.grpc.generated.MerchantDetailsServiceGrpc;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.grpc.client.GrpcChannelFactory;
 
+/**
+ * Конфигурация gRPC-клиентов микросервиса merchant-details.
+ */
 @Configuration
 public class GrpcConfig {
 
+    private static final String API_MERCHANT_DETAILS_CHANNEL = "api-merchant-details";
+
+    /**
+     * Stub для получения реквизитов мерчанта.
+     */
     @Bean
     public MerchantDetailsServiceGrpc.MerchantDetailsServiceFutureStub merchantDetailsServiceFutureStub(
             GrpcChannelFactory channelFactory) {
-        Channel channel = channelFactory.createChannel("api-merchant-details");
+        Channel channel = channelFactory.createChannel(API_MERCHANT_DETAILS_CHANNEL);
         return MerchantDetailsServiceGrpc.newFutureStub(channel);
+    }
+
+    /**
+     * Stub для работы с конфигурациями мерчантов.
+     */
+    @Bean
+    public MerchantConfigServiceGrpc.MerchantConfigServiceFutureStub merchantConfigServiceFutureStub(
+            GrpcChannelFactory channelFactory) {
+        Channel channel = channelFactory.createChannel(API_MERCHANT_DETAILS_CHANNEL);
+        return MerchantConfigServiceGrpc.newFutureStub(channel);
     }
 
 }
