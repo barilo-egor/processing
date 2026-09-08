4577d88d08637c286737368ff2a9a0f4186325f9
vdovin <V.Vdovin@isida.by>
2026-09-08 14:54:13 +0300

    fix

--- processing-app/src/main/java/net/rcetech/Application.java
+++ processing-app/src/main/java/net/rcetech/Application.java
@@ -2,9 +2,8 @@ package net.rcetech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.data.redis.autoconfigure.DataRedisRepositoriesAutoConfiguration;
 
-@SpringBootApplication(exclude = DataRedisRepositoriesAutoConfiguration.class)
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
