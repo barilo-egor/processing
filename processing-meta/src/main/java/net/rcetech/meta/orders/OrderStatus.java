2d2beb072cdbc683fd448dce1ef326488c362e2b
barilo <barilo.egor@gmail.com>
2026-09-11 14:39:39 +0300

    добавлен OrderStatus в словарь

--- processing-meta/src/main/java/net/rcetech/meta/orders/OrderStatus.java
+++ processing-meta/src/main/java/net/rcetech/meta/orders/OrderStatus.java
@@ -1,8 +1,15 @@
 package net.rcetech.meta.orders;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public enum OrderStatus {
-    NEW,
-    CANCELED,
-    TIMEOUT,
-    SUCCESS
+    NEW("Новый"),
+    CANCELED("Отменен"),
+    TIMEOUT("Таймаут"),
+    SUCCESS("Успешный");
+
+    private final String description;
 }
