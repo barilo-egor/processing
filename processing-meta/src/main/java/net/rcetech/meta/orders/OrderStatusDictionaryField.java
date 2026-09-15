ff48d2ed1322ac70e5eee60bc25ddbae17d777d9
barilo <barilo.egor@gmail.com>
2026-09-15 19:25:34 +0300

    очепятка

--- processing-meta/src/main/java/net/rcetech/meta/orders/OrderStatusDictionaryField.java
+++ processing-meta/src/main/java/net/rcetech/meta/orders/OrderStatusDictionaryField.java
@@ -11,7 +11,7 @@ import java.util.Map;
 public class OrderStatusDictionaryField implements DictionaryField {
     @Override
     public String getField() {
-        return "OderStatus";
+        return "OrderStatus";
     }
 
     @Override
