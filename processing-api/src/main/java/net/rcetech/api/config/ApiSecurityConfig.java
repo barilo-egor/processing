1dd3c82d411d692cebd599a1bf908c81cb63c4f9
barilo <barilo.egor@gmail.com>
2026-09-20 14:17:34 +0300

    обновлена коллекция, добавлен метод получения ордеров клиентом

--- processing-api/src/main/java/net/rcetech/api/config/ApiSecurityConfig.java
+++ processing-api/src/main/java/net/rcetech/api/config/ApiSecurityConfig.java
@@ -1,6 +1,7 @@
 package net.rcetech.api.config;
 
 import net.rcetech.domain.service.clients.ApiKeyService;
+import net.rcetech.meta.WebPath;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -18,8 +19,13 @@ public class ApiSecurityConfig {
     @Bean
     @Order(2)
     public SecurityFilterChain apiSecurityFilterChain(HttpSecurity http, ApiKeyService apiKeyService) {
-        http.securityMatcher("/api/v1/**")
-                .addFilterBefore(new ApiKeyAuthenticationFilter(apiKeyService), UsernamePasswordAuthenticationFilter.class);
+        http.securityMatcher(WebPath.V1_API_PATH + "/**")
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers(WebPath.V1_API_PATH + "/**").hasRole("CLIENT"))
+                .addFilterBefore(
+                        new ApiKeyAuthenticationFilter(apiKeyService),
+                        UsernamePasswordAuthenticationFilter.class
+                );
         return http.build();
     }
 }
